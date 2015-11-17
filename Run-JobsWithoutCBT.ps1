aabe1222cb8ccd7af43385ed6f4b6cf54acc7fb4
cajeeper <jbennett@m-oit-7vqgw12-w.msjcad.edu>
2015-11-17 14:48:48 -0800

    Master

---
+++ Run-JobsWithoutCBT.ps1
@@ -0,0 +1,27 @@
+#Launch the Veeam PowerShell Plugins
+. "C:\Program Files\Veeam\Backup and Replication\Backup\Install-VeeamToolkit.ps1"
+
+#Gather VMware Jobs
+$Jobs = Get-VBRJob | ? { $_.BackupPlatform -match "VMware" }
+
+#Run each job with change tracking disabled, then re-enable
+foreach ($Job in $Jobs) {
+	#If job is running, wait for it to complete before moving on
+	while(!$JobStopped) {	
+			Get-VBRJob -Name $Job.name | ? { $_.IsRunning -ne $true } | % { $JobStopped = $true}
+			Sleep -Seconds 10
+		}
+	
+	#Disable CBT and start the job
+	$Job | Set-VBRJobAdvancedViOptions -UseChangeTracking $false | out-null
+	$Job | Start-VBRJob | %{ $JobStopped = $false }
+	
+	
+	while(!$JobStopped) {	
+			Get-VBRJob -Name $Job.name | ? { $_.IsRunning -ne $true } | % { $JobStopped = $true }
+			Sleep -Seconds 10
+		}
+	#Disable CBT after the job has stopped.
+	$Job | Set-VBRJobAdvancedViOptions -UseChangeTracking $true | out-null
+
+}
