ff7d04cafe03868d9fb36db35e1ba52c53432f11
cajeeper <jbennett@m-oit-7vqgw12-w.msjcad.edu>
2015-11-17 16:22:57 -0800

    Master

--- Run-JobsWithoutCBT.ps1
+++ Run-JobsWithoutCBT.ps1
@@ -7,6 +7,7 @@ $Jobs = Get-VBRJob | ? { $_.BackupPlatform -match "VMware" }
 #Run each job with change tracking disabled, then re-enable
 foreach ($Job in $Jobs) {
 	#If job is running, wait for it to complete before moving on
+	$JobStopped = $false
 	while(!$JobStopped) {	
 			Get-VBRJob -Name $Job.name | ? { $_.IsRunning -ne $true } | % { $JobStopped = $true}
 			Sleep -Seconds 10
@@ -14,15 +15,16 @@ foreach ($Job in $Jobs) {
 	
 	#Disable CBT and start the job
 	$Job | Set-VBRJobAdvancedViOptions -UseChangeTracking $false | out-null
-	$Job | Start-VBRJob | %{ $JobStopped = $false }
+	$Job | Start-VBRJob | out-null
 	
+	$JobStopped = $false
 	#Wait for the job to complete running before moving on
 	while(!$JobStopped) {	
 			Get-VBRJob -Name $Job.name | ? { $_.IsRunning -ne $true } | % { $JobStopped = $true }
 			Sleep -Seconds 10
 		}
 	
-	#Enable CBT after the job has stopped
+	#Enable CBT after the job has stopped.
 	$Job | Set-VBRJobAdvancedViOptions -UseChangeTracking $true | out-null
 
 }
