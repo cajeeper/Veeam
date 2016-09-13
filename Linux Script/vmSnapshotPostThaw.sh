8b86421f07286bc785955c1a3282175aa08c3f7e
cajeeper <cajeeper@gmail.com>
2016-09-13 11:46:48 -0700

    Master

---
+++ Linux Script/vmSnapshotPostThaw.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Post-Thaw: `date +%Y-%m-%d_%H:%M:%S:%N`" >>  /tmp/freeze-thaw.log
---
+++ Linux Script/vmSnapshotPreFreeze.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Pre-Freeze: `date +%Y-%m-%d_%H:%M:%S:%N`" >> /tmp/freeze-thaw.log
